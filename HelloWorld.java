fbb186e9b4c21afcc6b8761445524e7a71b016a1
Rayan Dutta <rondutta23@gmail.com>
2024-09-16 21:09:55 +0530

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    static int add(int a, int b){
+        return a+b;
+    }
+
+    public static void main(String args[]) throws IOException{
+        System.out.println(add(3,5));
+    }
+
+}
